478db1d8f94a248fd323e9174eb9a649e4ba182c
fdl <2432114474@qq.com>
2022-10-12 23:20:23 +0800

    邮件服务

---
+++ 1.Utility/MyEmailUtility/Class1.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+using System.Net.Mail;
+using System.Text;
+
+namespace MyEmailUtility
+{
+    public interface IMyEmailUtility
+    {
+        bool Send(string toaddress, string title, string context);
+    }
+    internal class Imp : IMyEmailUtility
+    {
+
+        public string FromAddress = "3045034329@qq.com";
+        public string Key = "wmdkeipkhuwndghg";
+        public string Host = "smtp.qq.com";
+        public bool Send(string toaddress, string title, string context)
+        {
+
+
+            //实例化两个必要的
+            MailMessage mail = new MailMessage();
+            SmtpClient smtp = new SmtpClient();
+
+            //发送邮箱地址
+            mail.From = new MailAddress(FromAddress);
+
+            //收件人(可以群发)
+            mail.To.Add(new MailAddress(toaddress));
+
+            //是否以HTML格式发送
+            mail.IsBodyHtml = true;
+            //主题的编码格式
+            mail.SubjectEncoding = Encoding.UTF8;
+            //邮件的标题
+            mail.Subject = title;
+            //内容的编码格式
+            mail.BodyEncoding = Encoding.UTF8;
+            //邮件的优先级
+            mail.Priority = MailPriority.Normal;
+            //发送内容,带一个图片标签,用于对方打开之后,回发你填写的地址信息
+            mail.Body = context;
+            //收件人可以在邮件里面
+            //mail.Headers.Add("Disposition-Notification-To", "回执信息");
+
+            //发件邮箱的服务器地址
+            smtp.Host = Host;
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtp.Timeout = 1000000;
+            //设置端口,如果不设置的话,默认端口为25
+            // smtp.Port = 465;
+            smtp.UseDefaultCredentials = false;
+            //验证发件人的凭据
+            smtp.Credentials = new System.Net.NetworkCredential(FromAddress, Key);
+            //是否为SSL加密
+            smtp.EnableSsl = true;
+
+            try
+            {
+                //发送邮件
+                smtp.Send(mail);
+                return true;
+            }
+            catch (Exception e1)
+            {
+                return false;
+            }
+        }
+    }
+    static public class MyEmailExtension
+    {
+        static public void UseMyEmail(this IServiceCollection services, string jwtkey)
+        {
+            services.TryAddSingleton<IMyEmailUtility,Imp>();
+        }  
+    }
+}
---
+++ 99.Test/Test1/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+(new MyEmailUtility.Imp() ).Send("2432114474@qq.com","title","context");
