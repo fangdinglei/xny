9a0e5cd9521a4d04d29f2c3e21a19048b296b8cd
fdl <2432114474@qq.com>
2022-10-17 20:13:48 +0800

    站内信

---
+++ 3.Client/View/FInternalMail.cs
@@ -0,0 +1,50 @@
+﻿using FdlWindows.View;
+using GrpcMain.Device;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyClient.View
+{
+    [AutoDetectView("FInternalMail", "站内信", "", true)]
+    public partial class FInternalMail : Form, IView
+    {
+        List<ValueTuple<long, string>>? dvs;
+        DeviceService.DeviceServiceClient deviceServiceClient;
+        public FInternalMail(DeviceService.DeviceServiceClient deviceServiceClient)
+        {
+            InitializeComponent();
+            this.deviceServiceClient = deviceServiceClient;
+        }
+
+
+     
+        public Control View => this;
+
+        public void OnEvent(string name, params object[] pars)
+        {
+
+        }
+
+        public void PrePare(params object[] par)
+        {
+            
+        }
+
+        public void SetViewHolder(IViewHolder viewholder)
+        {
+
+        }
+
+        public void OnTick()
+        {
+
+        }
+    } 
+}
