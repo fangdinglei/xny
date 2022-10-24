162e865670ca056fdc30712631c49251cefa7e42
fdl <2432114474@qq.com>
2022-10-24 22:29:25 +0800

    设备维修表

---
+++ 2.Sever/DBContext/MainContext/Device/Device_Repair.cs
@@ -0,0 +1,42 @@
+﻿//Add-Migration [--context MainContext]
+//Remove-Migration 取消最近一次迁移
+//Update-Database [迁移名称  迁移直到(包含)或回退直到(不回退指定的版本) 0表示一开始]
+//Drop-Database 
+//Get-Help about_EntityFrameworkCore
+//get-help Add-Migration
+//dotnet tool install --global dotnet-ef
+//dotnet ef -h
+//
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyDBContext.Main
+{
+    /// <summary>
+    /// 设备维修记录表
+    /// </summary>
+    public class Device_Repair : IHasCreator
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        public long DeviceId { get; set; }
+        public virtual Device Device { get; set; }
+
+        /// <summary>
+        /// 设备损坏发现时间
+        /// </summary>
+        public long DiscoveryTime { get; set; }
+        /// <summary>
+        /// 设备维修完成时间
+        /// </summary>
+        public long CompletionTime { get; set; }
+        /// <summary>
+        /// 维修详情
+        /// </summary>
+        public string Context { get; set; }
+
+        public long CreatorId { get; set; }
+        public virtual User Creator { get; }
+
+    }
+}
