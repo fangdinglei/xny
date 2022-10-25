260ade586b617706a9f8014d5cc8069846de0659
fdl <2432114474@qq.com>
2022-10-25 16:34:52 +0800

    本地数据功能

--- 3.Client/LocalDataBase.cs
+++ 3.Client/LocalDataBase.cs
@@ -4,6 +4,7 @@ using GrpcMain.UserDevice;
 using static GrpcMain.Account.DTODefine.Types;
 using GrpcMain.Device ;
 using GrpcMain.UserDevice ;
+using GrpcMain.Account;
 
 namespace MyClient
 {
@@ -11,18 +12,19 @@ namespace MyClient
         public long UserId;
         public Dictionary<long, Device> devices = new();
 
-        public List<UserInfo> Users = new();
+        public Dictionary<long, UserInfo> Users = new();
         public List<TypeInfo> TypeInfo = new();
         public List<DeviceWithUserDeviceInfo> DeviceWithUserDeviceInfos = new();
         public List<User_Device_Group> User_Device_Groups = new();
 
-
+        AccountService.AccountServiceClient _accountServiceClient;
         DeviceService.DeviceServiceClient _deviceServiceClient;
         UserDeviceService.UserDeviceServiceClient _userDeviceServiceClient;
-        public LocalDataBase(DeviceService.DeviceServiceClient deviceServiceClient, UserDeviceService.UserDeviceServiceClient userDeviceServiceClient)
+        public LocalDataBase(DeviceService.DeviceServiceClient deviceServiceClient, UserDeviceService.UserDeviceServiceClient userDeviceServiceClient, AccountService.AccountServiceClient accountServiceClient)
         {
             _deviceServiceClient = deviceServiceClient;
             _userDeviceServiceClient = userDeviceServiceClient;
+            _accountServiceClient = accountServiceClient;
         }
 
         public Device GetDevice(long id ) {
@@ -45,5 +47,36 @@ namespace MyClient
             }
            
         }
+        public void Save(Device value)
+        {
+            devices[value.Id] = value;  
+        }
+
+        public UserInfo UserInfo(long id)
+        {
+            if (Users.ContainsKey(id))
+                return Users[id];
+            try
+            {
+                var dv = _accountServiceClient.GetUserInfo (new Request_GetUserInfo()
+                {
+                    UserId=id,
+                    SubUser=false,
+                });
+                if (dv.UserInfo.Count==0  )
+                    return null;
+                Users[id] = dv.UserInfo[0];
+                return Users[id];
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+
+        }
+        public void Save(UserInfo value) {
+            Users[value.ID] = value;
+        }
+
     }
 }
